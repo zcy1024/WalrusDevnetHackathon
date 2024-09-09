57ec3029cb021e3aab322d4b2f7b612f241a0177
Zcy <zcy_chao1024@163.com>
2024-09-09 21:58:59 +0800

    shoot bullets

---
+++ frontend/src/components/Game/Bullet/index.tsx
@@ -0,0 +1,115 @@
+import { useState, useEffect, useRef } from "react"
+
+import "./index.css"
+
+type Props = {
+    left: string,
+    top: string
+}
+
+type bulletItem = {
+    bullet: HTMLDivElement,
+    st_x: number,
+    st_y: number,
+    x_v: number,
+    y_v: number,
+    cre_ms: number
+}
+
+// bullets
+let bullets: bulletItem[] = []
+
+export default function Bullet({ left, top }: Props) {
+    const bulletRef = useRef<HTMLDivElement | null>(null)
+
+    const sleep = (ms: number) => {
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+
+    // check bullet is ready
+    const [ready, setReady] = useState<boolean>(false)
+
+    const refresh = async() => {
+        if (ready)
+            return
+
+        await sleep(200)
+        setReady(true)
+
+        // shoot
+        const childNode = document.createElement("div")
+        childNode.style.position = "absolute"
+        childNode.style.left = left
+        childNode.style.top = top
+        childNode.style.height = "2vh"
+        childNode.style.width = "2vh"
+        childNode.style.border = "1px solid white"
+        childNode.style.borderRadius = "50%"
+        childNode.style.backgroundColor = "gray"
+        bulletRef.current?.appendChild(childNode)
+
+        bullets.push({
+            bullet: childNode,
+            st_x: Number(childNode.style.left.slice(0, -2)),
+            st_y: Number(childNode.style.top.slice(0, -2)),
+            x_v: Math.random(),
+            y_v: Math.random(),
+            cre_ms: new Date().getTime()
+        })
+
+        await sleep(200)
+
+        bullets = bullets.filter(bullet => {
+            const x = Number(bullet.bullet.style.left.slice(0, -2))
+            const y = Number(bullet.bullet.style.top.slice(0, -2))
+            const inner = x >= 0 && x <= 100 && y >= 0 && y <= 100
+            if (!inner)
+                bulletRef.current?.removeChild(bullet.bullet)
+            return inner
+        })
+
+        await sleep(200)
+        setReady(false)
+    }
+
+    useEffect(() => {
+        refresh()
+    }, [ready])
+
+    // bullets actions
+    const bulletsActions = () => {
+        let cur_ms = new Date().getTime()
+
+        // change speed
+        const change = (v: number) => {
+            v -= 0.5
+            if (v >= 0)
+                v += 1
+            else
+                v -= 1
+            return v
+        }
+
+        bullets.map(bullet => {
+            const tr_x_v = change(bullet.x_v)
+            const tr_y_v = change(bullet.y_v)
+            const time = (cur_ms - bullet.cre_ms) / 100
+            bullet.bullet.style.left = (bullet.st_x + tr_x_v * time).toString() + "vw"
+            bullet.bullet.style.top = (bullet.st_y + tr_y_v * time).toString() + "vh"
+        })
+
+        window.requestAnimationFrame(bulletsActions)
+    }
+    // run actions
+    useEffect(() => {
+        const idx = window.requestAnimationFrame(bulletsActions)
+        return () => {
+            window.cancelAnimationFrame(idx)
+        }
+    }, [])
+
+    return (
+        <div className="bullet" ref={bulletRef}>
+        </div>
+    )
+}
--- frontend/src/components/Game/index.tsx
+++ frontend/src/components/Game/index.tsx
@@ -1,6 +1,8 @@
-import { useRef, useEffect } from "react"
+import { useRef, useEffect, useState } from "react"
 
 import "./index.css"
+
+import Bullet from "./Bullet"
 
 export default function Game() {
     const playerRef = useRef<HTMLDivElement | null>(null)
@@ -11,11 +13,16 @@ export default function Game() {
     }
 
     // player
-    const initX = random()
-    const initY = random()
+    // const initX = random()
+    // const initY = random()
+    const [initPos, setInitPos] = useState<{x: string, y: string}>({x: random(), y: random()})
+
+    // ready to shoot
+    const [ready, setReady] = useState<boolean>(false)
 
     // keyboardevent
     useEffect(() => {
+        setReady(true)
         document.addEventListener("keydown", movePlayer, false)
         return () => {
             document.removeEventListener("keydown", movePlayer, false)
@@ -30,16 +37,21 @@ export default function Game() {
         } else if (e.key === "a" || e.key === "ArrowLeft") {
             style.left = (Math.max(0, Number(style.left.slice(0, -2)) - speed)).toString() + "vw"
         } else if (e.key === "s" || e.key === "ArrowDown") {
-            style.top = (Math.min(100 - Number(style.height.slice(0, -2)) - 0.5, Number(style.top.slice(0, -2)) + speed)).toString() + "vh"
+            style.top = (Math.min(100 - Number(style.height.slice(0, -2)) + 2.7, Number(style.top.slice(0, -2)) + speed)).toString() + "vh"
         } else if (e.key === "d" || e.key === "ArrowRight") {
-            style.left = (Math.min(100 - Number(style.height.slice(0, -2)) + 2, Number(style.left.slice(0, -2)) + speed)).toString() + "vw"
+            style.left = (Math.min(100 - Number(style.height.slice(0, -2)) + 4, Number(style.left.slice(0, -2)) + speed)).toString() + "vw"
         }
+        setInitPos({
+            x: style.left.slice(0, -2),
+            y: style.top.slice(0, -2)
+        })
     }
 
 
     return (
         <div>
-            <div className="circle" style={{height: "6vh", width: "6vh", left: `${initX}vw`, top: `${initY}vh`}} ref={playerRef}></div>
+            <div className="circle" style={{height: "6vh", width: "6vh", left: `${initPos.x}vw`, top: `${initPos.y}vh`}} ref={playerRef}></div>
+            { ready && <Bullet left={initPos.x + "vw"} top={initPos.y + "vh"}/> }
         </div>
     )
 }
