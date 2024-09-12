47afff5c4e6bd28f68c000de70657a2f154c0a54
Zcy <zcy_chao1024@163.com>
2024-09-12 20:48:15 +0800

    Fixed an issue where the first player could not be listed on the list

--- frontend/src/components/Game/Settlement/index.tsx
+++ frontend/src/components/Game/Settlement/index.tsx
@@ -96,8 +96,8 @@ export default function Settlement({ score, rank }: { score: number, rank: RankP
     const account = useCurrentAccount()
 
     useEffect(() => {
-        let fake_users = rank.users
-        let fake_scores = rank.scores
+        let fake_users = [...rank.users]
+        let fake_scores = [...rank.scores]
         while (fake_users.length < 10) {
             fake_users.push('------------------------------------------------------------------')
             fake_scores.push('-')
@@ -110,7 +110,7 @@ export default function Settlement({ score, rank }: { score: number, rank: RankP
     }, [rank])
 
     useEffect(() => {
-        setRegister(rank.scores.find(s => Number(s) < score) !== undefined)
+        setRegister(rank.scores.length === 0 || rank.scores.find(s => Number(s) < score) !== undefined)
     }, [score, rank])
 
     return (
