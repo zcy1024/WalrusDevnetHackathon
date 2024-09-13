3cb418f191d1bd6bf3e299b4c649fb9b866238a1
Zcy <zcy_chao1024@163.com>
2024-09-13 18:02:18 +0800

    update the lastest URL on Walrus

--- frontend/src/components/ids.ts
+++ frontend/src/components/ids.ts
@@ -1,6 +1,6 @@
 export const NetWork = "testnet"
 export const NeedBalance = 666666
-export const PackageID = "0xf8a68bda0000f7f5d4542d98e180fb6b6b9878817b18e184ab1c81723298883f"
-export const Publisher = "0x2cff394af947bdf150883598ee463742a31cd7ef30645aa050da834b996605b4"
-export const Income = "0x8b542d1f1860f32185c1bea6abed180ee83a6bb852e5a50f04914cc8c88608d6"
-export const RankList = "0xe19b7655ed7bc03708d8c8bafc369447f1b969656a078e45a57afa1a847579b6"
+export const PackageID = "0x376bb2eac064012f4bc69e5a083693a3f1edfef2287e21f5480b14b9727e432a"
+export const Publisher = "0xa8cc284f2e110bed4f19ca63752c5ba5f097fc929ea17b3241ea0a6964ddf5bd"
+export const Income = "0xc554f6f828091cc0ce50ac49061c502b4a258a95865cdf668abe563b5766d37b"
+export const RankList = "0x14c592054f5ee4b8c918abaa3a0fde131d60db3c96cdc8936c441d72c961d64f"
